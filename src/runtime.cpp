b57444c3498c795996f41f9ad28272da817ea0f8
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-02-06 22:16:25 +0100

    Update angelscript lib with new addons

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -1,10 +1,14 @@
 #include "runtime.h"
 #include "resource.h"
 #include "helpers/module.h"
-#include "angelscript/addon/scriptstdstring/scriptstdstring.h"
-#include "angelscript/addon/scripthelper/scripthelper.h"
 #include "helpers/docs.h"
 #include "helpers/events.h"
+#include "angelscript/addon/scriptstdstring/scriptstdstring.h"
+#include "angelscript/addon/scripthelper/scripthelper.h"
+#include "angelscript/addon/scriptarray/scriptarray.h"
+#include "angelscript/addon/scriptdictionary/scriptdictionary.h"
+#include "angelscript/addon/scriptmath/scriptmath.h"
+#include "angelscript/addon/scriptany/scriptany.h"
 
 AngelScriptRuntime::AngelScriptRuntime()
 {
@@ -12,7 +16,14 @@ AngelScriptRuntime::AngelScriptRuntime()
 
     engine = asCreateScriptEngine();
     engine->SetMessageCallback(asFUNCTION(Helpers::MessageHandler), 0, asCALL_CDECL);
+
+    // Register add-ons
     RegisterStdString(engine);
+    RegisterStdStringUtils(engine);
+    RegisterScriptArray(engine, true);
+    RegisterScriptDictionary(engine);
+    RegisterScriptMath(engine);
+    RegisterScriptAny(engine);
 
     // Create docs
     Helpers::DocsGenerator altGen("alt");
