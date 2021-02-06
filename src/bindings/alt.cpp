3c4d155b0e1118e66729fc5b931a00ae591dda9b
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-02-06 11:20:02 +0100

    Change function signatures

--- src/bindings/alt.cpp
+++ src/bindings/alt.cpp
@@ -57,20 +57,20 @@ static void On(std::string& event, asIScriptFunction* callback)
 static ModuleExtension altExtension("alt", [](asIScriptEngine* engine, DocsGenerator* docs)
 {
     // Generic
-    FUNCDEF("void GenericCallback()", "Generic callback without arguments or return value");
 
     // Logging
-    GLOBAL_FUNC("void log(string msg)", Log, "Logs the specified message to the console");
-    GLOBAL_FUNC("void logWarning(string msg)", LogWarning, "Logs the specified message as a warning to the console");
-    GLOBAL_FUNC("void logError(string msg)", LogError, "Logs the specified message as an error to the console");
+    GLOBAL_FUNC("void log(const string &in msg)", Log, "Logs the specified message to the console");
+    GLOBAL_FUNC("void logWarning(const string &in msg)", LogWarning, "Logs the specified message as a warning to the console");
+    GLOBAL_FUNC("void logError(const string &in msg)", LogError, "Logs the specified message as an error to the console");
 
     // Timers
-    GLOBAL_FUNC("uint setTimeout(GenericCallback@ callback, uint timeout)", SetTimeout, "Sets a timeout");
-    GLOBAL_FUNC("uint setInterval(GenericCallback@ callback, uint interval)", SetInterval, "Sets a interval");
-    GLOBAL_FUNC("uint nextTick(GenericCallback@ callback)", NextTick, "Sets a next tick handler");
-    GLOBAL_FUNC("uint everyTick(GenericCallback@ callback)", EveryTick, "Sets a every tick handler");
+    FUNCDEF("void TimerCallback()", "Callback used for timers");
+    GLOBAL_FUNC("uint setTimeout(TimerCallback@ callback, uint timeout)", SetTimeout, "Sets a timeout");
+    GLOBAL_FUNC("uint setInterval(TimerCallback@ callback, uint interval)", SetInterval, "Sets a interval");
+    GLOBAL_FUNC("uint nextTick(TimerCallback@ callback)", NextTick, "Sets a next tick handler");
+    GLOBAL_FUNC("uint everyTick(TimerCallback@ callback)", EveryTick, "Sets a every tick handler");
     GLOBAL_FUNC("void clearTimer(uint timerId)", ClearTimer, "Clears specified timer");
 
     // Event handling
-    GLOBAL_FUNC("void on(string event, GenericCallback@ callback)", On, "Registers a event handler");
+    //GLOBAL_FUNC("void on(string event, GenericCallback@ callback)", On, "Registers a event handler");
 });
