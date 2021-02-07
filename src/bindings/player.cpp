ad3b71395770efd355b8cc463d406c4f14f8afbd
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-02-07 22:15:25 +0100

    Fix worldobject getposition

--- src/bindings/player.cpp
+++ src/bindings/player.cpp
@@ -32,8 +32,7 @@ static ModuleExtension playerExtension("alt", [](asIScriptEngine* engine, DocsGe
 
     REGISTER_PROPERTY_WRAPPER_GET("Player", "string", "name", GetName);
     REGISTER_PROPERTY_WRAPPER_SET("Player", "uint", "model", SetModel);
-    REGISTER_PROPERTY_WRAPPER_SET("Player", "string", "model", SetModel);
-
+    
     REGISTER_METHOD_WRAPPER("Player", "void Spawn(float x, float y, float z, uint delay)", SpawnPlayer<float>);
     REGISTER_METHOD_WRAPPER("Player", "void Spawn(int x, int y, int z, uint delay)", SpawnPlayer<int>);
 
--- src/bindings/worldobject.h
+++ src/bindings/worldobject.h
@@ -5,10 +5,10 @@
 #include "vector3.h"
 
 template<class T>
-static Helpers::Vector3<float>* GetPosition(T* obj)
+static Vector3<float> GetPosition(T* obj)
 {
     auto pos = obj->GetPosition();
-    return new Helpers::Vector3<float>(pos.x, pos.y, pos.z);
+    return Vector3<float>(pos.x, pos.y, pos.z);
 }
 
 template<class T>
@@ -39,11 +39,9 @@ namespace Helpers
         RegisterAsBaseObject<T>(engine, docs, type);
 
         // todo: fix this
-        /*
         REGISTER_PROPERTY_WRAPPER_GET(type, "Vector3f", "position", GetPosition<T>);
+        REGISTER_PROPERTY_WRAPPER_SET(type, "Vector3f", "position", SetPosition<T>);
         REGISTER_METHOD_WRAPPER(type, "Vector3f GetPosition()", GetPosition<T>);
-        REGISTER_METHOD_WRAPPER(type, "void SetPosition(float x, float y, float z)", SetPosition<T>);
-        */
 
         REGISTER_PROPERTY_WRAPPER_GET(type, "int", "dimension", GetDimension<T>);
         REGISTER_PROPERTY_WRAPPER_SET(type, "int", "dimension", SetDimension<T>);
