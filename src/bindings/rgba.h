6e21fb362b8dc6049bcb807c23757e0fe8341b0c
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-02-14 16:04:17 +0100

    Add RGBA class

---
+++ src/bindings/rgba.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Log.h"
+#include "../helpers/module.h"
+
+using namespace Helpers;
+
+static void Construct(uint8_t r, uint8_t g, uint8_t b, uint8_t a, void* memory)
+{
+    new(memory) alt::RGBA(r, g, b, a);
+}
+
+namespace Helpers
+{
+    static void RegisterRGBA(asIScriptEngine* engine, DocsGenerator* docs)
+    {
+        REGISTER_VALUE_CLASS("RGBA", alt::RGBA, asOBJ_VALUE | asOBJ_POD, "R(ed), G(reen), B(lue), A(lpha) color");
+        REGISTER_CONSTRUCTOR("RGBA", "uint8 r, uint8 g, uint8 b, uint8 a", Construct);
+        REGISTER_PROPERTY("RGBA", "uint8 r", alt::RGBA, r);
+        REGISTER_PROPERTY("RGBA", "uint8 g", alt::RGBA, g);
+        REGISTER_PROPERTY("RGBA", "uint8 b", alt::RGBA, b);
+        REGISTER_PROPERTY("RGBA", "uint8 a", alt::RGBA, a);
+        REGISTER_METHOD("RGBA", "RGBA toBGRA()", alt::RGBA, toBGRA);
+    }
+}
