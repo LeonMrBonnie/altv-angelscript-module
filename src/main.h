5c2e37c2686aa8fcf69399c9e75be631f9efc220
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-03-21 23:49:21 +0100

    v1.0.0

--- src/main.h
+++ src/main.h
@@ -2,8 +2,10 @@
 
 // Uncomment to activate debug mode (shows benchmarks)
 //#define DEBUG_MODE
+// Uncomment to generate docs files
+//#define AS_GENERATE_DOCUMENTATION
 
-#define MODULE_VERSION "0.0.1"
+#define MODULE_VERSION "1.0.0"
 #define MODULE_TYPE "as"
 
 // SDK
