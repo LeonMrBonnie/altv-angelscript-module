571a00b128b2514a0a77ff0b6833c3b01f8f23d6
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-04-08 16:29:49 +0200

    Add missing values to HTTP error enum

--- src/bindings/addons/http/http.cpp
+++ src/bindings/addons/http/http.cpp
@@ -10,6 +10,18 @@ static ModuleExtension httpExtension("http", [](asIScriptEngine* engine, DocsGen
     // Error enum
     REGISTER_ENUM("Error", "A HTTP request error");
     REGISTER_ENUM_VALUE("Error", "SUCCESS", httplib::Error::Success);
+    REGISTER_ENUM_VALUE("Error", "UNKNOWN", httplib::Error::Unknown);
+    REGISTER_ENUM_VALUE("Error", "CONNECTION", httplib::Error::Connection);
+    REGISTER_ENUM_VALUE("Error", "BIND_IP_ADDRESS", httplib::Error::BindIPAddress);
+    REGISTER_ENUM_VALUE("Error", "READ", httplib::Error::Read);
+    REGISTER_ENUM_VALUE("Error", "WRITE", httplib::Error::Write);
+    REGISTER_ENUM_VALUE("Error", "EXCEED_REDIRECT_COUNT", httplib::Error::ExceedRedirectCount);
+    REGISTER_ENUM_VALUE("Error", "CANCELED", httplib::Error::Canceled);
+    REGISTER_ENUM_VALUE("Error", "SSL_CONNECTION", httplib::Error::SSLConnection);
+    REGISTER_ENUM_VALUE("Error", "SSL_LOADING_CERTS", httplib::Error::SSLLoadingCerts);
+    REGISTER_ENUM_VALUE("Error", "SSL_SERVER_VERIFICATION", httplib::Error::SSLServerVerification);
+    REGISTER_ENUM_VALUE("Error", "UNSUPPORTED_MULTIPART_BOUNDARY_CHARS", httplib::Error::UnsupportedMultipartBoundaryChars);
+    REGISTER_ENUM_VALUE("Error", "COMPRESSION", httplib::Error::Compression);
 
     // Result class
     REGISTER_VALUE_CLASS("Result", httplib::Result, asOBJ_VALUE | asOBJ_POD, "A result from a HTTP request");
