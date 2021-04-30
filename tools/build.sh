890d6536f932ce6f7bbc55d3953cfaa3f53d5483
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-04-30 22:00:18 +0200

    Fix linux build output directory

--- tools/build.sh
+++ tools/build.sh
@@ -11,6 +11,9 @@ cd ..
 if [ ! -d "./dist" ]; then
     mkdir dist
 fi
+if [ ! -d "./dist/server" ]; then
+    mkdir dist/server
+fi
 
-cp ./build/linux64/server/libangelscript-module.so dist
+cp ./build/linux64/server/libangelscript-module.so dist/server
 #cp build\linux64\Release\angelscript-module.pdb dist
