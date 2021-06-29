f360436284a4c92e2789d1e148cadb9f2b6826a5
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-06-29 16:59:44 +0200

    Add clientside vehicle events

--- src/events/vehicle.cpp
+++ src/events/vehicle.cpp
@@ -55,3 +55,42 @@ REGISTER_EVENT_HANDLER(alt::CEvent::Type::NETOWNER_CHANGE,
                            return context->Execute();
                        });
 #endif
+
+#ifdef CLIENT_MODULE
+REGISTER_EVENT_HANDLER(alt::CEvent::Type::PLAYER_ENTER_VEHICLE,
+                       VehicleEnter,
+                       "void",
+                       "Vehicle@ vehicle",
+                       [](AngelScriptResource* resource, const alt::CEvent* event, asIScriptContext* context) {
+                           auto ev = static_cast<const alt::CPlayerEnterVehicleEvent*>(event);
+
+                           context->SetArgObject(0, ev->GetTarget().Get());
+                           return context->Execute();
+                       });
+
+REGISTER_EVENT_HANDLER(alt::CEvent::Type::PLAYER_LEAVE_VEHICLE,
+                       VehicleLeave,
+                       "void",
+                       "Vehicle@ vehicle",
+                       [](AngelScriptResource* resource, const alt::CEvent* event, asIScriptContext* context) {
+                           auto ev = static_cast<const alt::CPlayerLeaveVehicleEvent*>(event);
+
+                           context->SetArgObject(0, ev->GetTarget().Get());
+                           return context->Execute();
+                       });
+
+REGISTER_EVENT_HANDLER(alt::CEvent::Type::PLAYER_CHANGE_VEHICLE_SEAT,
+                       VehicleSeatChange,
+                       "void",
+                       "Vehicle@ vehicle, uint8&in newSeat, uint8&in oldSeat",
+                       [](AngelScriptResource* resource, const alt::CEvent* event, asIScriptContext* context) {
+                           auto ev = static_cast<const alt::CPlayerChangeVehicleSeatEvent*>(event);
+
+                           context->SetArgObject(0, ev->GetTarget().Get());
+                           auto seat = ev->GetNewSeat();
+                           context->SetArgAddress(1, &seat);
+                           auto oldSeat = ev->GetOldSeat();
+                           context->SetArgAddress(2, &oldSeat);
+                           return context->Execute();
+                       });
+#endif
