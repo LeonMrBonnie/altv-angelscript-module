bca2bf776f90e0a406621edf1573eaaac46e632b
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-06-30 02:56:31 +0200

    Add implicit conversion of string to Hash

--- src/bindings/data/hash.cpp
+++ src/bindings/data/hash.cpp
@@ -2,7 +2,14 @@
 
 using namespace Helpers;
 
+static uint32_t StringToHash(const std::string& str)
+{
+    return alt::ICore::Instance().Hash(str);
+}
+
 static DataExtension hashExtension([](asIScriptEngine* engine, DocsGenerator* docs) {
     // Common joaat hash
     REGISTER_TYPEDEF("Hash", "uint32");
+
+    engine->RegisterObjectMethod("string", "uint32 opImplConv() const", asFunctionPtr(StringToHash), asCALL_CDECL_OBJFIRST);
 });
