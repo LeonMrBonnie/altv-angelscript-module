4820e52e67e219ce62da310f36c81f0a85b6e718
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-08-30 14:36:43 +0200

    Remove ClangFormat as build target

--- tools/format.sh
+++
@@ -1,9 +0,0 @@
-cd ..
-
-if [ ! -d "./build" ]; then
-    mkdir build
-fi
-cd build
-cmake -DSERVER_MODULE=1 -DCLIENT_MODULE=1 -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --target clangformat
-cd ..
