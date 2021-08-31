485c435af2bc21aa4f49a9d0995d391f8b3446a4
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-08-31 16:37:21 +0200

    Add type check to conversion from entity to superclass

--- src/bindings/entities/entity.cpp
+++ src/bindings/entities/entity.cpp
@@ -1,8 +1,13 @@
 #include "entity.h"
 
-template<class T>
+template<class T, alt::IBaseObject::Type type>
 static T* Cast(alt::IEntity* entity)
 {
+    if(entity->GetType() != type)
+    {
+        THROW_ERROR("Entity has wrong type");
+        return nullptr;
+    }
     return dynamic_cast<T*>(entity);
 }
 
@@ -10,6 +15,6 @@ static ModuleExtension entityExtension("alt", [](asIScriptEngine* engine, DocsGe
     RegisterAsEntity<alt::IEntity>(engine, docs, "Entity");
 
     // Casts
-    REGISTER_METHOD_WRAPPER("Entity", "alt::Player@ opCast() const", Cast<alt::IPlayer>);
-    REGISTER_METHOD_WRAPPER("Entity", "alt::Vehicle@ opCast() const", Cast<alt::IVehicle>);
+    REGISTER_METHOD_WRAPPER("Entity", "alt::Player@ opCast() const", (Cast<alt::IPlayer, alt::IBaseObject::Type::PLAYER>));
+    REGISTER_METHOD_WRAPPER("Entity", "alt::Vehicle@ opCast() const", (Cast<alt::IVehicle, alt::IBaseObject::Type::VEHICLE>));
 });
