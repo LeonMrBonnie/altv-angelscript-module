a4a0c834e82a2abf8b25eb27130987b86cf5db32
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-09-16 19:30:10 +0200

    Extract debug config into seperate CMake file

---
+++ cmake/DebugConfig.cmake
@@ -0,0 +1,8 @@
+# Debug config
+
+# todo: toggle this via server.cfg later
+
+#add_definitions(-DDEBUG_MODE)                 # General debug features
+#add_definitions(-DSHOW_PROFILE)               # Profiling
+#add_definitions(-DDEBUG_MEMORY)               # Memory debugging
+#add_definitions(-DGENERATE_DOCUMENTATION)     # Documentation generation
